a77140f611267940552f4ef1f07eb3ba4af14223
andre25eng <31853449+andre25eng@users.noreply.github.com>
2018-09-25 14:48:27 +0200

    Utvecklat våra formulär och Kontrolerat inputs. Sedan rensade mellanslag

--- htdocs/kap_6/upg_6_1/upg_6_1.php
+++ htdocs/kap_6/upg_6_1/upg_6_1.php
@@ -1,13 +1,18 @@
-/* Gör ett formulär där användaren ska fylla i namn, adress, postnr och postort.
-
-Kontrollera att alla fälten är ifyllda, och innehåller minst 3 tecken.
-Kontrollera att postnumret innehåller 5 tecken och att de tecknen endast är siffror. */
+<?php
+/*
+* PHP version 7
+* @category   Förmulär
+* @author     Dre
+* @license    PHP CC
+*/
+?>
 <!DOCTYPE html>
 <html lang="sv">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Formulärcheck</title>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.0/dist/mini-default.min.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
@@ -19,29 +24,44 @@ Kontrollera att postnumret innehåller 5 tecken och att de tecknen endast är si
         $postort = $_POST["postort"];
     
         if (strlen($namn) == 0){
-            echo "<p>Varning Vg fyll i Namnet.</p>";
+            echo "<p>Varning Vänligeng fyll i Namnet.</p>";
         }
         if (strlen($adress) == 0){
-            echo "<p>Varning Vg fyll i Adress.</p>";
+            echo "<p>Varning Vänligeng fyll i Adress.</p>";
         }
         if (strlen($postnr) == 0){
-            echo "<p>Varning Vg fyll i Postnr.</p>";
+            echo "<p>Varning Vänligeng fyll i giltigt Postnr.</p>";
         }
         if (strlen($postort) == 0){
-            echo "<p>Varning Vg fyll i Postort.</p>";
+            echo "<p>Varning Vänligeng fyll i Postort.</p>";
         }
     
+        if (strlen($namn) <= 3){
+            echo "Varning för kort namn!";
+        }
+        if (strlen($adress) <= 3){
+            echo "Varning adressen är för kort!";
+        }
+        if (strlen($postnr) <= 3){
+            echo "Varning postnr för kort!";
+        }
+        if (strlen($postort) <= 3){
+            echo "Varning postort för kort!";
+        }
     
-    
-    
-    
+        if (strlen($postort) != 5){
+            echo "Postnummer måste inehålla 5 nummer!";
+        }
+        if (!ctype_digit($postnr)) {
+            echo "Får endast inehålla sifror!";
+        }
     
     }
     ?>
     <form action="#" method="post">
         <label for="">Namn</label><input type="text" name="namn"><br>
         <label for="">Adress</label><input type="text" name="adress"><br>
-        <label for="">Postnr</label><input type="text" name="postnr"><br>
+        <label for="">Postnr</label><input type="number" name="postnr"><br>
         <label for="">Postort</label><input type="text" name="postort"><br>
         <button>Skicka in</button>
 </body>
---
+++ htdocs/kap_6/upg_6_2/upg_6_2.php
@@ -0,0 +1,120 @@
+<?php
+/*
+* PHP version 7
+* @category   Förmulär
+* @author     Dre
+* @license    PHP CC
+*/
+?>
+<!DOCTYPE html>
+<html lang="sv">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Formulärcheck</title>
+    <link rel="stylesheet" href="https://cdn.rawgit.com/Chalarangelo/mini.css/v3.0.0/dist/mini-default.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php
+    if  (isset($_POST["namn"]) && isset($_POST["adress"]) && isset($_POST["postnr"]) && isset($_POST["postort"])&& isset($_POST["epost"])){
+        $namn = $_POST["namn"];
+        $adress = $_POST["adress"];
+        $postnr = $_POST["postnr"];
+        $postort = $_POST["postort"];
+        $epost = $_POST["epost"];
+        $fel = 0;
+    
+        if (strlen($namn) == 0){
+            echo "<p>Varning Vänligeng fyll i Namnet.</p>";
+            $fel++;
+        }
+        if (strlen($adress) == 0){
+            echo "<p>Varning Vänligeng fyll i Adress.</p>";
+            $fel++;
+        }
+        if (strlen($postnr) == 0){
+            echo "<p>Varning Vänligeng fyll i giltigt Postnr.</p>";
+            $fel++;
+        }
+        if (strlen($postort) == 0){
+            echo "<p>Varning Vänligeng fyll i Postort.</p>";
+            $fel++;
+        }
+        if (strlen($epost) == 0){
+            echo "<p>Varning Vänligeng fyll i Epost.</p>";
+            $fel++;
+        }
+    
+
+        if (strlen($namn) <= 3){
+            echo "<p>Varning för kort namn!</p>";
+            $fel++;
+        }
+        if (strlen($adress) <= 3){
+            echo "<p>Varning adressen är för kort!</p>";
+            $fel++;
+        }
+        if (strlen($postnr) <= 3){
+            echo "<p>Varning postnr för kort!</p>";
+            $fel++;
+        }
+        if (strlen($postort) <= 3){
+            echo "<p>Varning postort för kort!</p>";
+            $fel++;
+        }
+        if (strlen($epost) <= 3){
+            echo "<p>Varning epost för kort!</p>";
+            $fel++;
+        }
+    
+
+        if (strlen($postnr) != 5){
+            echo "<p>Postnummer måste inehålla 5 nummer!</p>";
+            $fel++;
+        }
+        if (!ctype_digit($postnr)) {
+            echo "<p>Får endast inehålla sifror!</p>";
+            $fel++;
+        }
+        if (strlen($epost) <= 6){
+            echo "<p>Epost måste inehålla minst 6 tecken!</p>";
+            $fel++;
+        }
+        if (!strpos($epost, '@')) {
+            echo "<p>Epost måste inehålla @</p>";
+            $fel++;
+        }
+        if (!strpos($epost, '.')) {
+            echo "<p>Epost måste inehålla misnt 1 punkt!</p>";
+            $fel++;
+        }
+
+
+        if ($fel == 0) {
+?>
+    <form action="#" method="post">
+        <label for="">Namn</label><input type="text" name="namn"><br>
+        <label for="">Adress</label><input type="text" name="adress"><br>
+        <label for="">Postnr</label><input type="number" name="postnr"><br>
+        <label for="">Postort</label><input type="text" name="postort"><br>
+        <label for="">Epost</label><input type="text" name="epost"><br>
+        <button>Skicka in</button>
+    </form>
+<?php
+        } else {
+?>
+    <form action="#" method="post">
+        <label for="">Namn</label><input type="text" name="namn" value="<?php echo $namn ?>"><br>
+        <label for="">Adress</label><input type="text" name="adress" value="<?php echo $adress ?>"><br>
+        <label for="">Postnr</label><input type="number" name="postnr" value="<?php echo $postnr ?>"><br>
+        <label for="">Postort</label><input type="text" name="postort" value="<?php echo $postort ?>"><br>
+        <label for="">Epost</label><input type="text" name="epost" value="<?php echo $epost ?>"><br>
+        <button>Skicka in</button>
+    </form>
+<?php
+        }
+    }
+?>
+</body>
+</html>
